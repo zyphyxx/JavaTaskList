1562147878e11927afb6de2e112f981a13c71979
Edgar Jesus <mendesbr92@gmail.com>
2024-03-27 22:30:06 -0300

    closes #3 - funcionalidades de entrada e saida

--- TaskListApp.java
+++ TaskListApp.java
@@ -1,7 +1,84 @@
+import java.util.Scanner;
+
 public class TaskListApp {
+
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        // exibir o menu
+        exibirMenu();
+        // adicionar
+        //exibir
+        //remover
+        //sair
     }
+
+    private static void exibirMenu () {
+
+        int valor;
+
+        do {
+
+            System.out.println("==== Task List App ===");
+            System.out.println();
+            System.out.println("1- Adicionar tarefas");
+            System.out.println("2- Exibir tarefas");
+            System.out.println("3- Remover tarefas");
+            System.out.println("4- Sair");
+            System.out.println();
+            System.out.println(" Digite sua escolha: ");
+
+            valor = scanner.nextInt();
+
+            switch (valor) {
+                case 1: adicionarTarefas();
+                    break;
+                case 2: exibirTarefas();
+                    break;
+                case 3: removerTarefa();
+                    break;
+
+
+                default:
+                    System.out.println("Digite uma opçao valida");
+
+            }
+
+        } while (valor != 4);
+
+        sair();
+
+
+    }
+
+    private static void adicionarTarefas () {
+        // TODO: 27/03/2024
+        System.out.println("adicionar tarefas");
+        scanner.close();
+
+    }
+
+    private static void exibirTarefas () {
+        // TODO: 27/03/2024
+        System.out.println("exibindo tarefas");
+        scanner.close();
+
+    }
+
+    private static void removerTarefa (){
+        // TODO: 27/03/2024
+        System.out.println("removendo tarefa");
+        scanner.close();
+    }
+
+    private static void sair () {
+        // TODO: 27/03/2024
+        System.out.println("ate a proxima");
+        scanner.close();
+
+    }
+
+
 }
+
